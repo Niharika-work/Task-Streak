9d993edd7e23d3bfc360aa77d2609268c82f1d9a
Niharika Sharma <niharika.sharma@xenonstack.com>
2024-02-13 01:14:36 +0530

    added changes to get data from server

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
-import logo from './logo.svg';
+import React, {useEffect, useRef, useState} from 'react';
 import './App.css';
 
 function App() {
+  const [usersData, setUsersData] = useState([{}]);
+  const inputRef = useRef();
+
+  useEffect(() => {
+    fetch("/api/users").then(
+      response => response.json()
+    ).then(
+      data => {
+        setUsersData(data);
+      }
+    )
+  }, []);
+
+  const addUsers = () => {
+    const data = {
+      name: inputRef.current.value
+    };
+    fetch("/api/users", {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then(
+      response => response.json()
+    ).then(
+      data => {
+        console.log('user added!', data)
+      }
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-      <div></div>
-      </header>
+      <div>
+      {usersData.users ? usersData.users.map((user, i) => (
+        <p>{user.name}</p>
+      )) :
+      <p>Loading...</p>}
+      ADD USER:
+      <input placeholder='enter name' ref={inputRef}/>
+      <button onClick={addUsers}>ADD</button>
+      </div>
     </div>
   );
 }
